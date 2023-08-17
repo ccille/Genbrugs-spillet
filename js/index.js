1e49fcce45bf13fb23414dc63161db5cddca3326
Cecilie Maria Hernig <ceci7542@edu.easj.dk>
2023-08-17 11:11:35 +0200

    hello

--- js/index.js
+++ js/index.js
@@ -1,11 +1,14 @@
 const App = Vue.createApp({
     data() {
         return {
-            
+            image: './Image/Beach.png',
+            images: './Image/google-play-badge.png',
         }
     },
     methods:{
-        
+        store(){
+            return "https://www.hawaiimagazine.com/the-5-best-beaches-in-hawaii-in-2021/";
+        }
     },
     computed: {
         
